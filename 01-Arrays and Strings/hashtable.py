c3c00375981d12f188c1cfd785a2f95aacc7c773
Harshvardhan Pandey <harshvardhanpandey@hotmail.com>
2018-03-19 02:34:45 -0500

    1.2

--- 01-Arrays and Strings/hashtable.py
+++ 01-Arrays and Strings/hashtable.py
@@ -5,8 +5,7 @@ def hash_val(key_str, hashtable_size):
 	# ord() function: 
 	# input: 1 character
 	# return: unicode of the character. ex: 'a' has unicode of 97
-    return sum([ord(c) for c in key_str]) % size
+    return sum([ord(c) for c in key_str]) % hashtable_size
 
 key = 'apple'
-print(hash_function(key, len(key)))
-
+print(hash_val(key, 15))
